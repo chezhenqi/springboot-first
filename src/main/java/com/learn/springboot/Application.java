e2406979f31bbd35e8d14ad05970cd48620829a4
che <1147491516@qq.com>
2018-06-19 14:54:10 +0800

    添加配置类

--- src/main/java/com/learn/springboot/Application.java
+++ src/main/java/com/learn/springboot/Application.java
@@ -2,8 +2,8 @@ package com.learn.springboot;
 
 import java.util.Properties;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import com.github.pagehelper.PageHelper;
 
 @SpringBootApplication
-@MapperScan("com.learn.springboot.mapper")
+@EnableAutoConfiguration
 @EnableTransactionManagement
 public class Application {
 	/**
